b036b70ede0f4b4f1c3a3334f69d8ef587d94995
Margaret <margaretmumbi@dynasoft.co.ke>
2021-10-05 16:19:45 +0300

    First Commit

---
+++ SchoolManagementUI/Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolMgtDbEntities db = new SchoolMgtDbEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginUser(string email, string password)
+        {
+            try
+            {
+
+                if (email != null && password != null)
+                {
+                    var finduser = db.UserTables.Where(u => u.EmailAddress == email && u.Password == password).ToList();
+                    if (finduser.Count() == 1)
+                    {
+                        Session["UserName"] = finduser[0].UserName;
+                        Session["Password"] = finduser[0].Password;
+                        Session["FullName"] = finduser[0].FullName;
+                        Session["EmailAddress"] = finduser[0].EmailAddress;
+                        Session["UserTypeID"] = finduser[0].UserTypeID;
+                        Session["ContactNo"] = finduser[0].ContactNo;
+                        Session["Address"] = finduser[0].Address;
+                        Session["UserID"] = finduser[0].UserID;
+
+                        //Admin
+                        //Operator
+                        //Teacher
+                        //Student
+
+                        string url = string.Empty;
+                        if (finduser[0].UserTypeID == 2)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 3)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 4)
+                        {
+                            return RedirectToAction("About");
+                        }
+                        else if (finduser[0].UserTypeID == 1)
+                        {
+                            url = "About";
+                        }
+                        else
+                        {
+                            url = "About";
+                        }
+                        return RedirectToAction(url);
+                    }
+
+                    else
+                    {
+                        Session["UserID"] = string.Empty;
+                        Session["UserTypeID"] = string.Empty;
+                        Session["FullName"] = string.Empty;
+                        Session["UserName"] = string.Empty;
+                        Session["Password"] = string.Empty;
+                        Session["ContactNo"] = string.Empty;
+                        Session["EmailAddress"] = string.Empty;
+                        Session["Address"] = string.Empty;
+                        ViewBag.message = "User Name and Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    Session["UserID"] = string.Empty;
+                    Session["UserTypeID"] = string.Empty;
+                    Session["FullName"] = string.Empty;
+                    Session["UserName"] = string.Empty;
+                    Session["Password"] = string.Empty;
+                    Session["ContactNo"] = string.Empty;
+                    Session["EmailAddress"] = string.Empty;
+                    Session["Address"] = string.Empty;
+                    ViewBag.message = "Some unexpected issue is occure please try again !";
+                }
+            }
+            catch (Exception ex)
+            {
+                Session["UserID"] = string.Empty;
+                Session["UserTypeID"] = string.Empty;
+                Session["FullName"] = string.Empty;
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["ContactNo"] = string.Empty;
+                Session["EmailAddress"] = string.Empty;
+                Session["Address"] = string.Empty;
+                ViewBag.message = "Some unexpected issue is occure please try again!";
+
+
+            }
+            return View("Login");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            Session["FullName"] = string.Empty;
+            Session["UserName"] = string.Empty;
+            Session["Password"] = string.Empty;
+            Session["ContactNo"] = string.Empty;
+            Session["EmailAddress"] = string.Empty;
+            Session["Address"] = string.Empty;
+
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
